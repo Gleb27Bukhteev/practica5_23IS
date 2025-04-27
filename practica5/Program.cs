d2c801a3cd52af7310d5889ab05a1b31f07db514
Gleb Bukhteev <glebbukhteev@gmail.com>
2025-04-27 23:16:48 +0300

    «Выполнен ввод значения х.»

--- practica5/Program.cs
+++ practica5/Program.cs
@@ -10,6 +10,9 @@ namespace practica5
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите значение x = ");
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
         }
     }
 }
